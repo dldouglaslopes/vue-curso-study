8688c6325d6b3bdc699f299046af54fa1b1dbee0
dldouglaslopes <dldouglaslopes@gmail.com>
2020-04-26 16:10:02 -0300

    desafio4

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,17 +1,47 @@
 new Vue({
 	el: '#desafio',
 	data: {
-		aplicarEfeito: false
+		classe1: 'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
+		cor5: '',
+		estilo5: {
+			width: '100px',
+			height: '100px'
+		},
+		barra6: {
+			backgroundColor:'red',
+			height: '50px',
+			width: '50px' 
+		},
+		contador6: 0
 	},
 	methods: {
 		iniciarEfeito() {
-			this.aplicarEfeito = !aplicarEfeito
-			return {
-				destaque: this.aplicarEfeito, encolher: !this.aplicarEfeito
-			}
+			setInterval(() => {
+				this.classe1 = this.classe1== 'destaque'?
+						'encolher' : 'destaque'
+			},1000)
 		},
 		iniciarProgresso() {
-
+			const temporizador = setInterval(() => {
+				if( this.contador6 != 8){
+					this.contador6++;
+					this.barra6.width = 50 * this.contador6 + 'px';
+				}
+				else{
+					clearInterval(temporizador)
+				}
+			},1000)
+		},
+		setPerigo(event){
+			if(event.target.value == "true"){
+				this.perigo = true
+			}
+			else if(event.target.value == "false"){
+				this.perigo = false
+			}
 		}
 	}
 })
