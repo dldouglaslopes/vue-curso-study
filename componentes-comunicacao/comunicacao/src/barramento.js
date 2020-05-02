ecb504c46fa33c45ccd8803cb6d558148f23fb00
dldouglaslopes <dldouglaslopes@gmail.com>
2020-05-02 17:23:16 -0300

    comunicacao desafio

--- componentes-comunicacao/comunicacao/src/barramento.js
+++ componentes-comunicacao/comunicacao/src/barramento.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 export default new Vue({
     methods: {
         alterarIdade(idade) {
-            this.$email('idadeMudou', idade)
+            this.$emit('idadeMudou', idade)
         },
         quandoIdadeMudar(callback){
             this.$on('idadeMudou', callback)
